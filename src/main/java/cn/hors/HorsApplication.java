4612d67c013b38fbe1ecd2b1d441c95f858a8fed
十 <947661959@qq.com>
2020-07-26 00:34:03 +0800

    基本框架

---
+++ src/main/java/cn/hors/HorsApplication.java
@@ -0,0 +1,13 @@
+package cn.hors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/hors/HorsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
