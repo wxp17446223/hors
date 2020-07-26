dde0ae5ce836cbc82d70dc597f8cae0280220c1d
十 <947661959@qq.com>
2020-07-26 14:36:14 +0800

    修改了一些映射

--- src/main/java/cn/hors/HorsApplication.java
+++ src/main/java/cn/hors/HorsApplication.java
@@ -1,8 +1,10 @@
 package cn.hors;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("cn.hors.mapper")
 @SpringBootApplication
 public class HorsApplication {
 
