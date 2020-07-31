976c3ab71e44dadd00d74513237e5a760610d8b4
LENOVO <506382185@qq.com>
2020-07-31 17:50:11 +0800

    tjh第5天

--- src/main/java/cn/hors/controller/RegisterController.java
+++ src/main/java/cn/hors/controller/RegisterController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class HomeController {
+@RequestMapping("/register")
+public class RegisterController {
 
     @GetMapping
-    public String home() {
-        return "index";
+    public String toRegister(){
+        return "register";
     }
 }
