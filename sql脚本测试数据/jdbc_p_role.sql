d5cb8ab72c6b443b768e8a48405291a8d06ddd6a
fish_sudo <2955456802@qq.com>
2020-08-01 14:25:44 +0800

    tjh第4天

---
+++ sql脚本测试数据/jdbc_p_role.sql
@@ -0,0 +1,4 @@
+INSERT INTO jdbc.p_role (id, name, code) VALUES (1, '管理员', 'admin');
+INSERT INTO jdbc.p_role (id, name, code) VALUES (2, '账号管理员', 'account');
+INSERT INTO jdbc.p_role (id, name, code) VALUES (3, '资源管理员', 'resource');
+INSERT INTO jdbc.p_role (id, name, code) VALUES (4, '角色管理员', 'role');
