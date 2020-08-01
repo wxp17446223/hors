2aa5e2937d596c7ce0a9b2cea86573dc5bc8a3c5
wxp17446223 <2544133573@qq.com>
2020-08-01 17:32:29 +0800

    123

--- sql脚本测试数据/jdbc_p_account.sql
+++ sql脚本测试数据/jdbc_p_account.sql
@@ -1,4 +1,4 @@
-INSERT INTO jdbc.p_account (id, account, password, head) VALUES (10, '123', '202cb962ac59075b964b07152d234b70', 'QQ图片20200523203655.jpg');
-INSERT INTO jdbc.p_account (id, account, password, head) VALUES (24, 'resource', '202cb962ac59075b964b07152d234b70', null);
-INSERT INTO jdbc.p_account (id, account, password, head) VALUES (26, 'account', '202cb962ac59075b964b07152d234b70', null);
-INSERT INTO jdbc.p_account (id, account, password, head) VALUES (29, 'role', '202cb962ac59075b964b07152d234b70', null);
+INSERT INTO p_account (id, account, password, head) VALUES (10, '123', '202cb962ac59075b964b07152d234b70', 'QQ图片20200523203655.jpg');
+INSERT INTO p_account (id, account, password, head) VALUES (24, 'resource', '202cb962ac59075b964b07152d234b70', null);
+INSERT INTO p_account (id, account, password, head) VALUES (26, 'account', '202cb962ac59075b964b07152d234b70', null);
+INSERT INTO p_account (id, account, password, head) VALUES (29, 'role', '202cb962ac59075b964b07152d234b70', null);
