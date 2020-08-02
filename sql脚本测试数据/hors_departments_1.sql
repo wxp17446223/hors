cc1f6503550fa30e1cf0d0d8b38b165f3c7d7fe3
fish_sudo <2955456802@qq.com>
2020-08-02 12:53:05 +0800

    tjh第4天

--- sql脚本测试数据/hors_departments_1.sql
+++ sql脚本测试数据/hors_departments_1.sql
@@ -1,27 +1,27 @@
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (1, '外科', '外科，治疗各种外科疾病', '2020-07-27', -1, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (2, '内科', '内科，治疗各种内科', '2020-07-27', -1, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (3, '泌尿外科', '属于外科，各种尿结石和复杂性肾结石；肾脏和膀胱肿瘤；前列腺增生和前列腺炎；睾丸附睾的炎症和肿瘤；睾丸精索鞘膜积液；各种泌尿系损伤；泌尿系先天性畸形如尿道下裂、隐睾、肾盂输尿管连接部狭窄所导致的肾积水等等。', '2020-07-28', 1, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (4, '肾内科', '属于内科，肾脏病是常见病和多发病，如果恶化可以发展为尿毒症，严重危害人们健康。慢性肾脏病已成为继心脑血管病、肿瘤、糖尿病之后又一种威胁人类健康的重要疾病，成为全球性公共卫生问题。', '2020-07-28', 2, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (5, '妇产科', '妇产科分为妇科、产科', '2020-07-27', -1, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (6, '儿科', '儿科，治疗各种儿科疾病', '2020-07-27', -1, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (7, '皮肤科', '皮肤科，治疗各种皮肤疾病', '2020-07-27', -1, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (8, '眼科', '眼科，治疗各种眼科疾病', '2020-07-27', -1, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (9, '耳鼻喉科', '耳鼻咽喉科，治疗各种耳鼻咽喉疾病', '2020-07-27', -1, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (10, '口腔科', '口腔科，治疗各种口腔疾病
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (1, '外科', '外科，治疗各种外科疾病', '2020-07-27', -1, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (2, '内科', '内科，治疗各种内科', '2020-07-27', -1, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (3, '泌尿外科', '属于外科，各种尿结石和复杂性肾结石；肾脏和膀胱肿瘤；前列腺增生和前列腺炎；睾丸附睾的炎症和肿瘤；睾丸精索鞘膜积液；各种泌尿系损伤；泌尿系先天性畸形如尿道下裂、隐睾、肾盂输尿管连接部狭窄所导致的肾积水等等。', '2020-07-28', 1, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (4, '肾内科', '属于内科，肾脏病是常见病和多发病，如果恶化可以发展为尿毒症，严重危害人们健康。慢性肾脏病已成为继心脑血管病、肿瘤、糖尿病之后又一种威胁人类健康的重要疾病，成为全球性公共卫生问题。', '2020-07-28', 2, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (5, '妇产科', '妇产科分为妇科、产科', '2020-07-27', -1, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (6, '儿科', '儿科，治疗各种儿科疾病', '2020-07-27', -1, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (7, '皮肤科', '皮肤科，治疗各种皮肤疾病', '2020-07-27', -1, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (8, '眼科', '眼科，治疗各种眼科疾病', '2020-07-27', -1, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (9, '耳鼻喉科', '耳鼻咽喉科，治疗各种耳鼻咽喉疾病', '2020-07-27', -1, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (10, '口腔科', '口腔科，治疗各种口腔疾病
 ', '2020-07-27', -1, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (11, '肿瘤科', '肿瘤科和内科、外科、妇产科和儿科一样，是临床医学的二级学科，分为肿瘤内科、肿瘤放射治疗科和肿瘤外科等，肿瘤内科主要从事各种良、恶性肿瘤的内科治疗；肿瘤放射治疗科主要从事肿瘤的放射线治疗；肿瘤外科提供以手术为主的综合治疗。专门的肿瘤医院的相关科室会根据不同部位再行细分，例如肿瘤内科里的胃肠肿瘤科、淋巴瘤肿瘤科；肿瘤外科有乳腺外科、头颈外科、胸外科、肿瘤妇科、腹部外科等。', '2020-07-27', -1, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (12, '中医科', '中医科采用中药治疗各种冠心病、心律失常、脑梗塞、脑动脉硬化、顽固性头痛、急慢性肾炎、泌尿系结石、男科病、脾胃病、糖尿病以及心身疾病。运用针灸、理疗等方法治疗中风、偏瘫、风湿性关节炎、哮喘等疾病，开设针灸减肥门诊，为广大肥胖患者减轻痛苦。微电脑牵引仪治疗颈椎病、腰椎增生及腰椎间盘突出症，效果显著', '2020-07-27', -1, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (13, '妇科', '妇科是医疗机构的一个诊疗科目，妇科是妇产科的一个分支专业，是以诊疗女性妇科病为诊疗的专业科室，分为西医妇科与中医妇科。妇科疾病包括：女性生殖系统的疾病即为妇科疾病，包括外阴疾病、阴道疾病、子宫疾病、输卵管疾病、卵巢疾病等', '2020-07-28', 5, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (14, '产科', '产科是临床医学四大主要学科之一，主要研究女性生殖器官疾病的病因、病理、诊断及防治，妊娠、分娩的生理和病理变化，高危妊娠及难产的预防和诊治，女性生殖内分分泌，计划生育及妇女保健等。', '2020-07-28', 5, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (15, '儿科门诊', '对儿童的进行分析的门诊部', '2020-07-28', 6, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (16, '儿科保健科', '对儿童的身心健康保健的科系', '2020-07-28', 6, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (17, '皮肤美容科', '皮肤美容科，通常指医学上，通过专业治疗与养生等方式，使人体皮肤达到美容保养的效果。皮肤美容，通常指一种改变原有的不良行为和疾病（面部或皮肤），使之成为文明的、高素质的、具有可以被人接受的外观形象有活动和过程，或为达到此目的而使用的产品和方法。', '2020-07-28', 7, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (18, '损伤性皮肤病科', '损伤性皮肤病科，进行皮肤的专业整治', '2020-07-28', 7, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (19, '眼科门诊', '眼科的全称是“眼病专科”，是研究发生在视觉系统，包括眼球及与其相关联的组织有关疾病的学科。眼科一般研究玻璃体、视网膜疾病，眼视光学，青光眼和视神经病变，白内障等多种眼科疾病。', '2020-07-28', 8, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (20, '口鼻喉科门诊', '“耳鼻咽喉科”是诊断治疗耳、鼻、咽、喉、及其相关头颈区域的外科学科。', '2020-07-28', 9, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (21, '口腔科门诊', '口腔科，医学学科分类之一。主要口腔科疾病包括：口腔颌面部皮样、表皮颌下间隙感染、颌面部淋巴管瘤、齿状突发育畸形、上颌窦恶性肿瘤、颌骨造釉细胞瘤、慢性筛窦炎、下颌后缩、四环素牙、舌白斑等疾病。通过技术，许多牙周病完全可以治愈。
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (11, '肿瘤科', '肿瘤科和内科、外科、妇产科和儿科一样，是临床医学的二级学科，分为肿瘤内科、肿瘤放射治疗科和肿瘤外科等，肿瘤内科主要从事各种良、恶性肿瘤的内科治疗；肿瘤放射治疗科主要从事肿瘤的放射线治疗；肿瘤外科提供以手术为主的综合治疗。专门的肿瘤医院的相关科室会根据不同部位再行细分，例如肿瘤内科里的胃肠肿瘤科、淋巴瘤肿瘤科；肿瘤外科有乳腺外科、头颈外科、胸外科、肿瘤妇科、腹部外科等。', '2020-07-27', -1, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (12, '中医科', '中医科采用中药治疗各种冠心病、心律失常、脑梗塞、脑动脉硬化、顽固性头痛、急慢性肾炎、泌尿系结石、男科病、脾胃病、糖尿病以及心身疾病。运用针灸、理疗等方法治疗中风、偏瘫、风湿性关节炎、哮喘等疾病，开设针灸减肥门诊，为广大肥胖患者减轻痛苦。微电脑牵引仪治疗颈椎病、腰椎增生及腰椎间盘突出症，效果显著', '2020-07-27', -1, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (13, '妇科', '妇科是医疗机构的一个诊疗科目，妇科是妇产科的一个分支专业，是以诊疗女性妇科病为诊疗的专业科室，分为西医妇科与中医妇科。妇科疾病包括：女性生殖系统的疾病即为妇科疾病，包括外阴疾病、阴道疾病、子宫疾病、输卵管疾病、卵巢疾病等', '2020-07-28', 5, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (14, '产科', '产科是临床医学四大主要学科之一，主要研究女性生殖器官疾病的病因、病理、诊断及防治，妊娠、分娩的生理和病理变化，高危妊娠及难产的预防和诊治，女性生殖内分分泌，计划生育及妇女保健等。', '2020-07-28', 5, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (15, '儿科门诊', '对儿童的进行分析的门诊部', '2020-07-28', 6, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (16, '儿科保健科', '对儿童的身心健康保健的科系', '2020-07-28', 6, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (17, '皮肤美容科', '皮肤美容科，通常指医学上，通过专业治疗与养生等方式，使人体皮肤达到美容保养的效果。皮肤美容，通常指一种改变原有的不良行为和疾病（面部或皮肤），使之成为文明的、高素质的、具有可以被人接受的外观形象有活动和过程，或为达到此目的而使用的产品和方法。', '2020-07-28', 7, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (18, '损伤性皮肤病科', '损伤性皮肤病科，进行皮肤的专业整治', '2020-07-28', 7, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (19, '眼科门诊', '眼科的全称是“眼病专科”，是研究发生在视觉系统，包括眼球及与其相关联的组织有关疾病的学科。眼科一般研究玻璃体、视网膜疾病，眼视光学，青光眼和视神经病变，白内障等多种眼科疾病。', '2020-07-28', 8, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (20, '口鼻喉科门诊', '“耳鼻咽喉科”是诊断治疗耳、鼻、咽、喉、及其相关头颈区域的外科学科。', '2020-07-28', 9, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (21, '口腔科门诊', '口腔科，医学学科分类之一。主要口腔科疾病包括：口腔颌面部皮样、表皮颌下间隙感染、颌面部淋巴管瘤、齿状突发育畸形、上颌窦恶性肿瘤、颌骨造釉细胞瘤、慢性筛窦炎、下颌后缩、四环素牙、舌白斑等疾病。通过技术，许多牙周病完全可以治愈。
 ', '2020-07-28', 10, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (22, '肿瘤科门诊', '肿瘤科和内科、外科、妇产科和儿科一样，是临床医学的二级学科，分为肿瘤内科、肿瘤放射治疗科和肿瘤外科等，肿瘤内科主要从事各种良、恶性肿瘤的内科治疗；肿瘤放射治疗科主要从事肿瘤的放射线治疗；肿瘤外科提供以手术为主的综合治疗。专门的肿瘤医院的相关科室会根据不同部位再行细分，例如肿瘤内科里的胃肠肿瘤科、淋巴瘤肿瘤科；肿瘤外科有乳腺外科、头颈外科、胸外科、肿瘤妇科、腹部外科等。', '2020-07-28', 11, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (23, '中医科门诊', '口腔科，医学学科分类之一。主要口腔科疾病包括：口腔颌面部皮样、表皮颌下间隙感染、颌面部淋巴管瘤、齿状突发育畸形、上颌窦恶性肿瘤、颌骨造釉细胞瘤、慢性筛窦炎、下颌后缩、四环素牙、舌白斑等疾病。通过技术，许多牙周病完全可以治愈。
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (22, '肿瘤科门诊', '肿瘤科和内科、外科、妇产科和儿科一样，是临床医学的二级学科，分为肿瘤内科、肿瘤放射治疗科和肿瘤外科等，肿瘤内科主要从事各种良、恶性肿瘤的内科治疗；肿瘤放射治疗科主要从事肿瘤的放射线治疗；肿瘤外科提供以手术为主的综合治疗。专门的肿瘤医院的相关科室会根据不同部位再行细分，例如肿瘤内科里的胃肠肿瘤科、淋巴瘤肿瘤科；肿瘤外科有乳腺外科、头颈外科、胸外科、肿瘤妇科、腹部外科等。', '2020-07-28', 11, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (23, '中医科门诊', '口腔科，医学学科分类之一。主要口腔科疾病包括：口腔颌面部皮样、表皮颌下间隙感染、颌面部淋巴管瘤、齿状突发育畸形、上颌窦恶性肿瘤、颌骨造釉细胞瘤、慢性筛窦炎、下颌后缩、四环素牙、舌白斑等疾病。通过技术，许多牙周病完全可以治愈。
 ', '2020-07-28', 12, '18899996666');
-INSERT INTO hors.departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (24, '针灸门诊', '属于中医科', '2020-07-28', 12, '18899996666');
+INSERT INTO departments (depart_id, depart_name, introduce, date, d_id, phone) VALUES (24, '针灸门诊', '属于中医科', '2020-07-28', 12, '18899996666');
