83f24473d734feb51ea3d3bc46806affc3381206
十 <947661959@qq.com>
2020-08-05 16:38:02 +0800

    新闻模块后台新增ueditor的实现

--- src/main/java/cn/hors/HorsApplication.java
+++ src/main/java/cn/hors/HorsApplication.java
@@ -1,11 +1,13 @@
 package cn.hors;
 
+import cn.jasonone.ueditor.EnableUeditor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("cn.hors.mapper")
 @SpringBootApplication
+@EnableUeditor
 public class HorsApplication {
     public static void main(String[] args) {
         SpringApplication.run(HorsApplication.class, args);
